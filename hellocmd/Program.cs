43e6e5cd30798868f182a8f05feb77635be9f56f
Conrad Andrew <Conradagandrew@gmail.com>
2019-07-28 19:45:01 +0100

    Add project files.

---
+++ hellocmd/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hellocmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello {0}, goodbye {1}", args[0], args[1]);
+        }
+    }
+}
